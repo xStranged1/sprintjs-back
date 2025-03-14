d1f10f343fbe43d0f66ac2fef9a1f6432e76aa47
xStranged <fede.valle04@gmail.com>
2025-03-14 18:48:38 -0300

    feat: :bug: migration

--- src/migration/restoreState.ts
+++ src/migration/restoreState.ts
@@ -1,10 +1,12 @@
 import { AppDataSource } from "@/config/data-source";
+import { Circuit } from "@/entities/Circuit";
 import { Sprint } from "@/entities/Sprint";
 import * as fs from "fs";
 
 export async function restoreState() {
     const sprintRepo = AppDataSource.getRepository(Sprint);
+    const circuitRepo = AppDataSource.getRepository(Circuit);
     const data = JSON.parse(fs.readFileSync("backup.json", "utf-8"));
-    await sprintRepo.save(data.circuits);
+    await circuitRepo.save(data.circuits);
     await sprintRepo.save(data.sprints);
 }
